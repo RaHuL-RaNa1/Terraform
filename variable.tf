56ca6b45367c851d44f840d67c8dba764ef4f739
RaHuL-RaNa1 <124451661+RaHuL-RaNa1@users.noreply.github.com>
2024-01-30 23:08:43 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,6 @@
 variable "region_aws" {
   default = "us-east-2"
 }
-variable "a_key" {}
+variable "a_key" {} //Access Key or Secret key is Null here so that we can use it Dyanamically
 
 variable "s_key" {}
