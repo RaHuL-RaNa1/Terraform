ecf7046bc92d9512fa0696573495195a94d724ee
RaHuL-RaNa1 <124451661+RaHuL-RaNa1@users.noreply.github.com>
2024-01-30 23:09:11 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 /*provider "aws" {
         region = "us-east-2"
-        access_key = "AKIA4AJZZJZHNCSSYTWD"
-        secret_key = "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"
+        access_key = "AKIA4AJZZJZHNCSSYD"
+        secret_key = "uDazzqx7sv9jGHTHVFhedZWQKTp3/hwY5Paxf"
 }*/
 resource "aws_instance" "myec2" {
-        ami = "ami-09694bfab577e90b0"
+        ami = "ami-09694b7e90b0"
         instance_type = "t2.micro"
         count = 2 //to create multiple instancestes
 }
